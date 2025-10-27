ad5cad9086f5117f7ed38eccfa083e37a58ddca4
Angel Chaico <angel20170238b@gmail.com>
2025-10-27 10:15:55 -0500

    6. Working with Images

--- src/main/java/com/company/onboarding/entity/User.java
+++ src/main/java/com/company/onboarding/entity/User.java
@@ -1,5 +1,6 @@
 package com.company.onboarding.entity;
 
+import io.jmix.core.FileRef;
 import io.jmix.core.HasTimeZone;
 import io.jmix.core.annotation.Secret;
 import io.jmix.core.entity.annotation.JmixGeneratedValue;
@@ -40,6 +41,9 @@ public class User implements JmixUserDetails, HasTimeZone {
     @Column(name = "JOINING_DATE")
     private LocalDate joiningDate;
 
+    @Column(name = "PICTURE", length = 1024)
+    private FileRef picture;
+
     @JoinColumn(name = "DEPARTMENT_ID")
     @ManyToOne(fetch = FetchType.LAZY)
     private Department department;
@@ -77,6 +81,14 @@ public class User implements JmixUserDetails, HasTimeZone {
 
     @Transient
     private Collection<? extends GrantedAuthority> authorities;
+
+    public FileRef getPicture() {
+        return picture;
+    }
+
+    public void setPicture(FileRef picture) {
+        this.picture = picture;
+    }
 
     public LocalDate getJoiningDate() {
         return joiningDate;
