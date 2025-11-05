b25d5bc2418b6816a0bdfc6ae2b6359e8b455694
Angel Chaico <angel20170238b@gmail.com>
2025-11-05 09:13:18 -0500

    modal create step and add steps items

--- src/main/java/com/company/onboarding/view/step/StepDetailView.java
+++ src/main/java/com/company/onboarding/view/step/StepDetailView.java
@@ -3,14 +3,12 @@ package com.company.onboarding.view.step;
 import com.company.onboarding.entity.Step;
 import com.company.onboarding.view.main.MainView;
 import com.vaadin.flow.router.Route;
-import io.jmix.flowui.view.EditedEntityContainer;
-import io.jmix.flowui.view.StandardDetailView;
-import io.jmix.flowui.view.ViewController;
-import io.jmix.flowui.view.ViewDescriptor;
+import io.jmix.flowui.view.*;
 
 @Route(value = "steps/:id", layout = MainView.class)
 @ViewController(id = "Step.detail")
 @ViewDescriptor(path = "step-detail-view.xml")
 @EditedEntityContainer("stepDc")
+@DialogMode(width = "40em", height = "auto", resizable = true) // controlar la anchura del form modal
 public class StepDetailView extends StandardDetailView<Step> {
 }
